3fd25d7194038519696b99d18d5fef890b96a1fa
weixia <544373054@qq.com>
2016-12-15 00:54:31 +0800

    init project

---
+++ app/src/main/java/com/example/asus1/weather0236/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.asus1.weather0236;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by asus1 on 2016/12/15.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
